c9b10f4bf7382ead2ab941160811c3760842b6d3
Riccardo Serafini <riccardo@befair.it>
2020-03-13 22:08:48 +0100

    export hosts.json to fill the table

---
+++ scrape_prometheus.py
@@ -0,0 +1,23 @@
+import urllib.request
+import json
+import time
+
+while True:
+    time.sleep(5)
+    instances = []
+    req = urllib.request.Request(url='http://prometheus:9090/api/v1/query?query=jitsi_participants')
+    with urllib.request.urlopen(req) as f:
+        data = json.loads(f.read().decode('utf-8'))
+
+    for server in data['data']['result']:
+        d = {}
+        d['name'] = server['metric']['instance'].split(':')[0]
+        d['user_count'] = server['value'][1]
+        d['by'] = server['metric']['jitsi_hosted_by']
+        d['by_url'] = server['metric']['jitsi_hosted_by_url']
+        d['url'] = server['metric']['jitsi_url']
+        instances.append(d)
+
+    with open('/hosts.json', 'w') as f:
+        f.write(json.dumps(instances, indent=2))
+
