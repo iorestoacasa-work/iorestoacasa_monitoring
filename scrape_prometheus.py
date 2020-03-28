ccf8369229963c3b19670f4163267f3af31302a9
Riccardo Serafini <riccardo@befair.it>
2020-03-28 22:35:23 +0100

    add core count and bandwidth in hosts.json

--- scrape_prometheus.py
+++ scrape_prometheus.py
@@ -21,12 +21,38 @@ while True:
         'PERSON': set(),
         'ASSOCIATION': set(),
     }
+
+    jitsi_required_labels = [
+        'instance',
+        'jitsi_hosted_by',
+        'jitsi_hosted_by_url'
+        'jitsi_url'
+        'jitsi_hosted_by_kind'
+        'software'
+        'available_bandwidth_mbps'
+        'core_count'
+    ]
+
+    mm_required_labels = [
+        'instance'
+        'url'
+        'hosted_by'
+        'hosted_by_url'
+        'hosted_by_kind'
+        'available_bandwidth_mbps'
+        'core_count'
+        'software'
+    ]
+
     participants_data = load_prometheus_query('http://prometheus:9090/api/v1/query?query=jitsi_participants')
     cpu_data = load_prometheus_query('http://prometheus:9090/api/v1/query?query=jitsi_cpu_usage')
     mm_data = load_prometheus_query('http://prometheus:9090/api/v1/query?query=probe_success{software="MM"}')
 
     for server in participants_data['data']['result']:
-        if 'jitsi_hosted_by_kind' in server['metric'] and 'software' in server['metric'] and server['metric']['software'] == 'JITSI':
+        for key in jitsi_required_labels:
+            if not key in server['metric']:
+                continue
+        if server['metric']['software'] == 'JITSI':
             d = {}
             d['name'] = clean_trailing_slash(server['metric']['instance'].split(':')[0])
             d['user_count'] = int(server['value'][1])
@@ -35,15 +61,23 @@ while True:
             d['url'] = clean_trailing_slash(server['metric']['jitsi_url'])
             d['by_kind'] = server['metric']['jitsi_hosted_by_kind']
             d['software'] = server['metric']['software']
+            d['available_bandwidth_mbps'] = server['metric']['available_bandwidth_mbps']
+            d['core_count'] = server['metric']['core_count']
             credits[d['by_kind']].add((d['by'], d['by_url']))
             instances[d['name']] = d
  
     for server in cpu_data['data']['result']:
-        if 'jitsi_hosted_by' in server['metric']:
+        for key in jitsi_required_labels:
+            if not key in server['metric']:
+                continue
+        if server['metric']['software'] == 'JITSI':
             name = clean_trailing_slash(server['metric']['instance'].split(':')[0])
             instances[name]['cpu_usage'] = round(float(server['value'][1]), ndigits=2)
 
     for server in mm_data['data']['result']:
+        for key in mm_required_labels:
+            if not key in server['metric']:
+                continue
         if server['metric'].get('software') == 'MM' and server['value'][1] == '1':
             d = {}
             d['name'] = clean_trailing_slash(server['metric']['instance'].replace('https://', ''))
@@ -52,6 +86,8 @@ while True:
             d['by_url'] = clean_trailing_slash(server['metric']['hosted_by_url'])
             d['by_kind'] = server['metric']['hosted_by_kind']
             d['software'] = server['metric']['software']
+            d['available_bandwidth_mbps'] = server['metric']['available_bandwidth_mbps']
+            d['core_count'] = server['metric']['core_count']
             credits[d['by_kind']].add((d['by'], d['by_url']))
             instances[d['name']] = d
 
